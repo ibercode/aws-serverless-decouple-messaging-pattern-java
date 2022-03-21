f8994309937902dfc80ac5f9a9062abe16a31ed6
razvan minciuna <razvan@razvans-MacBook-Pro-2.local>
2022-03-21 09:52:29 +0100

    Refactoring

--- src/main/java/com/ibercode/sales/Sales.java
+++ src/main/java/com/ibercode/sales/Sales.java
@@ -4,26 +4,27 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.ibercode.sales.utils.SalesDDBUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Sales implements RequestHandler<SQSEvent, String> {
 
-    private SalesDDBUtils ddbUtils = new SalesDDBUtils();
+    private final SalesDDBUtils ddbUtils = new SalesDDBUtils();
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sales.class);
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
 
-        String region = System.getenv("REGION");
         String tableName = System.getenv("SALES_TABLE");
 
         event.getRecords()
                 .forEach(
                         m -> {
                             String record = m.getBody();
-                            System.out.println("New record >" + record);
+                            LOGGER.info("[record]" + record);
                             String saleId = ddbUtils.putItem(record, tableName);
-                            System.out.println("[saleId] " + saleId);
+                            LOGGER.info("[saleId] " + saleId);
                         });
-
         return "Done";
     }
 }
